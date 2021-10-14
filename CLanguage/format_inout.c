3f34be768c5e024fbb6d77001da19ea641670731
Darwin1946 <darwin931369866@gmail.com>
2021-10-14 09:51:45 +0800

    commit in linux

---
+++ CLanguage/format_inout.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int number;
+ 
+    // printf() 
+    printf("input a integer: ");  
+    
+    // scanf() format input
+    scanf("%d", &number);  
+    
+    // printf() display input
+    printf("the input is: %d", number);
+    return 0;
+}
