925900408906939e719801aad7df931fabd8415d
Darwin1946 <darwin931369866@gmail.com>
2021-10-19 20:34:48 +0800

    push

--- CLanguage/C_Primer_Exercise/chapter3/transform.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(void)
-{
-    int input;
-    printf("input:");
-    scanf("%d",&input);
-    printf("dec=%d; octal=%o; hex=%x\n",input,input,input);
-    printf("dec=%#d; octal=%#o; hex=%#x\n",input,input,input);
-}
