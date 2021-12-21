fc8869dcb2a865c2c448af3afcc8b6e0d0429491
Darwin1946 <darwin931369866@gmail.com>
2021-12-21 23:38:12 +0800

    vs2022项目提交

---
+++ CCode/CCode/test.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()//主函数
+{
+	//char ch = 'a';
+	//printf("%c\n", ch);//打印字符%c
+
+	//int age = 20;
+	//printf("%d\n", age);//打印整形%d
+	//return 0;
+
+	//printf("%d\n",sizeof(char));      //1  
+	//printf("%d\n", sizeof(int));      //4
+	//printf("%d\n", sizeof(short));    //2
+	//printf("%d\n", sizeof(long));     //4
+	//printf("%d\n", sizeof(long long));//8
+	//printf("%d\n", sizeof(float));    //4
+	//printf("%d\n", sizeof(double));   //8
+
+	//float f = 95.6;
+	int num1 = 0;
+	int num2 = 0;
+	scanf_s("%d%d",&num1,&num2);
+	int sum = num1 + num2;
+	printf("%d",sum);
+	return 0;
+}
